952295bc765f3e636d946f1bf30d188dca796886
Kim Jin Young <116090051+jyk2022@users.noreply.github.com>
2024-06-14 17:20:31 +0900

    Update site.config.js
    change2

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "jyk!notion-blog",
+    description: "testing",
   },
 
   // CONFIG configration (required)
