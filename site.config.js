5cf6184e7fd44cf8d9b486ada9f84831b2016c52
Kim Jin Young <116090051+jyk2022@users.noreply.github.com>
2024-06-14 17:18:50 +0900

    Update site.config.js
    change

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "jinyoungsss",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "나다운 사람",
+    bio: "I useing computer",
+    email: "jykn23@naver.com",
+    linkedin: "linkjyk",
+    github: "jyk2022",
     instagram: "",
   },
   projects: [
